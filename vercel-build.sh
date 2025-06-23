42cee0a83ada56d1c743d4bfbd17ebae249be62c
Chiqueto <lfchiqueto@gmail.com>
2025-06-22 22:44:08 -0300

    fix: Adota script de build para deploy no Vercel

--- vercel-build.sh
+++ vercel-build.sh
@@ -4,6 +4,9 @@
 # Para a execução se ocorrer um erro
 set -e
 
+# Corrige o problema "fatal: bad object" que pode ocorrer no ambiente da Vercel
+git config --global fetch.fsckObjects false
+
 # Clona o SDK do Flutter na versão estável para uma pasta temporária
 echo "Clonando o SDK do Flutter..."
 git clone https://github.com/flutter/flutter.git --depth 1 --branch stable /tmp/flutter
