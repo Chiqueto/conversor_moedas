26aa002f6a8b029784d931abaab132a3ad07f5af
Chiqueto <lfchiqueto@gmail.com>
2025-06-22 22:40:54 -0300

    fix: Adota script de build para deploy no Vercel

---
+++ vercel-build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Este script instrui a Vercel a como instalar e compilar um projeto Flutter.
+
+# Para a execução se ocorrer um erro
+set -e
+
+# Clona o SDK do Flutter na versão estável para uma pasta temporária
+echo "Clonando o SDK do Flutter..."
+git clone https://github.com/flutter/flutter.git --depth 1 --branch stable /tmp/flutter
+export PATH="/tmp/flutter/bin:$PATH"
+
+# Executa o flutter doctor para verificar a instalação
+flutter doctor
+
+# Baixa as dependências do projeto
+echo "Baixando as dependências do projeto..."
+flutter pub get
+
+# Compila a versão web do projeto em modo 'release' para otimização
+echo "Compilando a aplicação web..."
+flutter build web --release
+
+echo "Build finalizado com sucesso!"
